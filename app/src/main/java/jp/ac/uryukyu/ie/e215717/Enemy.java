2953f7d0e80fbd3c1a1c0004bbba9b1bdf0c6f6c
taito takara <e215717@ie.u-ryukyu.ac.jp>
2021-11-13 22:34:33 +0900

    2nd commit

--- app/src/main/java/jp/ac/uryukyu/ie/e215717/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215717/Enemy.java
@@ -35,10 +35,9 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
-        if (hitPoint >= 0){
+       
             System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
-            hero.wounded(0);
-        }
+            hero.wounded(damage);
         
     }
 
