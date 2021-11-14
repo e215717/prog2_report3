fdf2dbed2917c5a0912cb0ba58fef85482938993
taito takara <e215717@ie.u-ryukyu.ac.jp>
2021-11-14 23:04:20 +0900

    2nd commit

--- app/src/main/java/jp/ac/uryukyu/ie/e215717/Hero.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215717/Hero.java
@@ -9,10 +9,10 @@ package jp.ac.uryukyu.ie.e215717;
  * Created by tnal on 2016/11/13.
  */
 public class Hero {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -27,7 +27,24 @@ public class Hero {
         dead = false;
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
+    
+    public void setName(String name){
+        this.name = name;
+    }
 
+    public String getName(){
+        return this.name;
+    }
+
+    
+
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
+
+    public boolean getDead(){
+        return this.dead;
+    }
     /**
      * Enemyへ攻撃するメソッド。
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
@@ -35,7 +52,7 @@ public class Hero {
      */
     public void attack(Enemy e){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, e.getName(), damage);
         e.wounded(damage);
     }
 
